a9313f9f8ebbd006bfdcf7d891a9830762c0fd5f
Alexandra <alexojha@gmail.com>
2014-06-01 21:40:15 +0100

    creating sample maven app

---
+++ src/main/java/com/ojha/App.java
@@ -0,0 +1,13 @@
+package com.ojha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
